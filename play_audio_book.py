eed2f60eb7248efa23fffc66461e7e17da6d38fa
Random Number Retailer LLC <randomnumberretailer@gmail.com>
2024-09-06 20:42:58 -0400

    added epub support. now you can skip a line with Control + z

--- play_audio_book.py
+++ play_audio_book.py
@@ -1,40 +1,57 @@
-from gtts import gTTS
 from striprtf.striprtf import rtf_to_text
+from epub_conversion.utils import open_book, convert_epub_to_lines
+from multiprocessing import Process
+from bs4 import BeautifulSoup
+from gtts import gTTS
 import time
 import os
 import json
 import sys
 import signal
-import subprocess
 import tempfile
+import subprocess
 
 # CONFIG ---------------------------------------------------------
 
 language = 'en'
-temp_mp3_filename = tempfile.NamedTemporaryFile().name
 progress_filename = 'audio_book_progress.json'
+temp_mp3_filename = tempfile.NamedTemporaryFile().name
 
 aggressive_saving = True
 seconds_between_lines = 0.5
+
+process = None
 
 # CODE ------------------------------------------------------------
 
 line_index_human_readable = 1
 book_location = os.path.abspath(sys.argv[1])
 
-def signal_handler(sig, frame):
+def graceful_exit(sig, frame):
     print('Exiting gracefully...')
     save_progress(book_location, line_index_human_readable)
     sys.exit(0)
 
-signal.signal(signal.SIGINT, signal_handler)
+def skip_line(sig, frame):
+    process.terminate()
 
-def read_sentence(text):
+signal.signal(signal.SIGINT, graceful_exit)
+signal.signal(signal.SIGTSTP, skip_line)
+
+def read_sentence_impl(text, language):
     myobj = gTTS(text=text, lang=language, slow=False)
 
     myobj.save(temp_mp3_filename)
 
     subprocess.call(['mpg123', '-q', temp_mp3_filename])
+
+def call_read_sentence(text):
+    global process
+    process = Process(target=read_sentence_impl, args=(text, language))
+    process.start()
+
+    while process.is_alive():
+        pass
 
     time.sleep(seconds_between_lines)
 
@@ -74,6 +91,13 @@ if __name__ == '__main__':
 
                 if line:
                    lines.append(line)
+    elif book_location.endswith('.epub'):
+        book = open_book(book_location)
+
+        lines = [
+            BeautifulSoup(line, features="html.parser").get_text()
+            for line in convert_epub_to_lines(book)
+        ]
     else:
         with open(book_location) as f:
             possible_lines = f.read().split('.')
@@ -93,7 +117,7 @@ if __name__ == '__main__':
             print(f'{line_index_human_readable}: {line}')
 
             try:
-                read_sentence(line)
+                call_read_sentence(line)
             except Exception as e:
                 print(e)
 
@@ -105,6 +129,6 @@ if __name__ == '__main__':
             print(f'{line_index_human_readable}: {line}')
 
             try:
-                read_sentence(line)
+                call_read_sentence(line)
             except Exception as e:
                 print(e)
